ac26fd13cf9eba668298d146a50c181716b48d62
Hinoto-LH <masseron.a.pro@gmail.com>
2026-05-11 14:08:20 +0200

    feat(node-js-basic): implement stdin interactive program

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+rl.on('line', (name) => {
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+rl.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
